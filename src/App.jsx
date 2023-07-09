9b45d84a4737e0eac6651cd9da751f722cd4cb91
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-07-09 12:53:01 +0200

    feature/phonebook

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+import styles from './App.module.css';
+
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleNumberChange = event => {
+    setNumber(event.target.value);
+  };
+
+  const handleAddContact = event => {
+    event.preventDefault();
+
+    const existingContact = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (existingContact) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    setContacts(prevContacts => [...prevContacts, newContact]);
+    setName('');
+    setNumber('');
+  };
+
+  const handleDeleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Phonebook</h1>
+
+      <ContactForm
+        name={name}
+        number={number}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+        onSubmit={handleAddContact}
+      />
+
+      <h2 className={styles.subheading}>Contacts</h2>
+
+      <Filter
+        value={filter}
+        onChange={event => setFilter(event.target.value)}
+      />
+
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
+
+export default App;
